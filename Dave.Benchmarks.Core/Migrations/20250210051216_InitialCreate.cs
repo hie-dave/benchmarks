20e984a9e511c9a57fbc08595e01f1902226801f
Drew Holzworth <d.holzworth@westernsydney.edu.au>
2025-02-11 09:26:03 +1100

    Update DB migrations

---
+++ Dave.Benchmarks.Core/Migrations/20250210051216_InitialCreate.cs
@@ -0,0 +1,347 @@
+using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dave.Benchmarks.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Datasets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    SpatialResolution = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TemporalResolution = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DatasetType = table.Column<string>(type: "varchar(13)", maxLength: 13, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Source = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Version = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    InputDataSource = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Parameters = table.Column<byte[]>(type: "longblob", nullable: true),
+                    Patches = table.Column<byte[]>(type: "longblob", nullable: true),
+                    ClimateDataset = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ModelVersion = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Datasets", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Pfts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pfts", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Variables",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Units = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Level = table.Column<int>(type: "int", nullable: false),
+                    DatasetId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Variables", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Variables_Datasets_DatasetId",
+                        column: x => x.DatasetId,
+                        principalTable: "Datasets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Individuals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Number = table.Column<int>(type: "int", nullable: false),
+                    DatasetId = table.Column<int>(type: "int", nullable: false),
+                    PftId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Individuals", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Individuals_Datasets_DatasetId",
+                        column: x => x.DatasetId,
+                        principalTable: "Datasets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Individuals_Pfts_PftId",
+                        column: x => x.PftId,
+                        principalTable: "Pfts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "VariableLayers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    VariableId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VariableLayers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VariableLayers_Variables_VariableId",
+                        column: x => x.VariableId,
+                        principalTable: "Variables",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Datum",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Value = table.Column<double>(type: "double", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Longitude = table.Column<double>(type: "double", nullable: false),
+                    Latitude = table.Column<double>(type: "double", nullable: false),
+                    VariableId = table.Column<int>(type: "int", nullable: false),
+                    LayerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Datum", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Datum_VariableLayers_LayerId",
+                        column: x => x.LayerId,
+                        principalTable: "VariableLayers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Datum_Variables_VariableId",
+                        column: x => x.VariableId,
+                        principalTable: "Variables",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "GridcellData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GridcellData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GridcellData_Datum_Id",
+                        column: x => x.Id,
+                        principalTable: "Datum",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "StandData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    StandId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StandData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StandData_GridcellData_Id",
+                        column: x => x.Id,
+                        principalTable: "GridcellData",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "PatchData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    PatchId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PatchData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PatchData_StandData_Id",
+                        column: x => x.Id,
+                        principalTable: "StandData",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "IndividualData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    IndividualId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IndividualData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IndividualData_Individuals_IndividualId",
+                        column: x => x.IndividualId,
+                        principalTable: "Individuals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IndividualData_PatchData_Id",
+                        column: x => x.Id,
+                        principalTable: "PatchData",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Datum_LayerId",
+                table: "Datum",
+                column: "LayerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Datum_VariableId",
+                table: "Datum",
+                column: "VariableId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IndividualData_IndividualId",
+                table: "IndividualData",
+                column: "IndividualId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Individuals_DatasetId_Number",
+                table: "Individuals",
+                columns: new[] { "DatasetId", "Number" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Individuals_PftId",
+                table: "Individuals",
+                column: "PftId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PatchData_PatchId",
+                table: "PatchData",
+                column: "PatchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pfts_Name",
+                table: "Pfts",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StandData_StandId",
+                table: "StandData",
+                column: "StandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VariableLayers_VariableId",
+                table: "VariableLayers",
+                column: "VariableId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Variables_DatasetId",
+                table: "Variables",
+                column: "DatasetId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IndividualData");
+
+            migrationBuilder.DropTable(
+                name: "Individuals");
+
+            migrationBuilder.DropTable(
+                name: "PatchData");
+
+            migrationBuilder.DropTable(
+                name: "Pfts");
+
+            migrationBuilder.DropTable(
+                name: "StandData");
+
+            migrationBuilder.DropTable(
+                name: "GridcellData");
+
+            migrationBuilder.DropTable(
+                name: "Datum");
+
+            migrationBuilder.DropTable(
+                name: "VariableLayers");
+
+            migrationBuilder.DropTable(
+                name: "Variables");
+
+            migrationBuilder.DropTable(
+                name: "Datasets");
+        }
+    }
+}
