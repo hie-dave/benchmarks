311ae7cc0a7772f41d51b035fb18e6913f30eba3
Drew Holzworth <drew@holzworth.info>
2025-05-15 16:33:02 +1000

    Added a now-redundant unit test
    (bug has been solved elsewhere now)

---
+++ Dave.Benchmarks.Tests/Services/OutputFileTypeResolverTests.cs
@@ -0,0 +1,35 @@
+using CommandLine;
+using Dave.Benchmarks.Core.Services;
+using LpjGuess.Runner.Parsers;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Dave.Benchmarks.Tests.Services;
+
+public class OutputFileTypeResolverTests
+{
+    private readonly OutputFileTypeResolver resolver;
+    private readonly Mock<ILogger<OutputFileTypeResolver>> logger;
+    private readonly Mock<InstructionFileParser> parser;
+
+    public OutputFileTypeResolverTests()
+    {
+        logger = new Mock<ILogger<OutputFileTypeResolver>>();
+        parser = new Mock<InstructionFileParser>();
+        resolver = new OutputFileTypeResolver(logger.Object);
+    }
+
+    [Fact]
+    public void EnsureQuotesAreStripped()
+    {
+        // TODO: mocking/better init
+        string insFile = @"
+file_lai ""lai.out""
+";
+        var parser = new InstructionFileParser(insFile, string.Empty);
+        resolver.BuildLookupTable(parser);
+
+        Assert.Equal("lai.out", resolver.GetFileName("file_lai"));
+        Assert.Equal("file_lai", resolver.GetFileType("lai.out"));
+    }
+}
